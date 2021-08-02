8aba49775100fca6fd2a7137976aa8d8d92220fc
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-08-02 20:03:19 +0200

    4. Use react-query for cache management

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,7 +2,16 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+// import {ReactQueryCacheProvider} from 'react-query'
 import {App} from './app'
+
+// function AppWithProviders() {
+//   return (
+//     <ReactQueryCacheProvider>
+//       <App />
+//     </ReactQueryCacheProvider>
+//   )
+// }
 
 loadDevTools(() => {
   ReactDOM.render(<App />, document.getElementById('root'))

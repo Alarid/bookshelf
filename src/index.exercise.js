880b3a411e9e0a09c60929b10d25b97e157fc99a
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-09-14 20:17:20 +0200

    4.2 - use global useQuery config

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,17 +2,28 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-// import {ReactQueryCacheProvider} from 'react-query'
+import {ReactQueryConfigProvider} from 'react-query'
 import {App} from './app'
 
-// function AppWithProviders() {
-//   return (
-//     <ReactQueryCacheProvider>
-//       <App />
-//     </ReactQueryCacheProvider>
-//   )
-// }
+const queryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      if (error.status === 404) return false
+      return failureCount < 2
+    },
+  },
+}
+
+function AppWithProviders() {
+  return (
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>
+  )
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(<AppWithProviders />, document.getElementById('root'))
 })

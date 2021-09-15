4c7fef76133e9fa7b26d181becff76d96db60626
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-09-15 19:36:35 +0200

    6.5 - prefetch book search query

--- src/screens/discover.exercise.js
+++ src/screens/discover.exercise.js
@@ -8,6 +8,7 @@ import {useBookSearch} from 'utils/books'
 import * as colors from 'styles/colors'
 import {BookRow} from 'components/book-row'
 import {BookListUL, Spinner, Input} from 'components/lib'
+import {refetchBookSearchQuery} from 'utils/books'
 
 function DiscoverBooksScreen({user}) {
   const [query, setQuery] = React.useState('')
@@ -22,6 +23,12 @@ function DiscoverBooksScreen({user}) {
     setQueried(true)
     setQuery(event.target.elements.search.value)
   }
+
+  React.useEffect(() => {
+    return () => {
+      refetchBookSearchQuery(user)
+    }
+  }, [user])
 
   return (
     <div>

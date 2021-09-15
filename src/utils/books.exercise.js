bfd2492da22248e1b1500e7dd3674b195967d5a9
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-09-15 19:56:10 +0200

    6.6 - add books to the query cache

--- src/utils/books.exercise.js
+++ src/utils/books.exercise.js
@@ -16,12 +16,22 @@ const loadingBooks = Array.from({length: 10}, (v, index) => ({
   ...loadingBook,
 }))
 
+const setQueryDataForBook = book =>
+  queryCache.setQueryData(['book', {bookId: book.id}], book)
+
 const getBookSearchConfig = (query, user) => ({
   queryKey: ['bookSearch', {query}],
   queryFn: () =>
     client(`books?query=${encodeURIComponent(query)}`, {
       token: user.token,
     }).then(data => data.books),
+  config: {
+    onSuccess(books) {
+      for (const book of books) {
+        setQueryDataForBook(book)
+      }
+    },
+  },
 })
 
 function useBookSearch(query, user) {
@@ -43,4 +53,4 @@ function refetchBookSearchQuery(user) {
   queryCache.prefetchQuery(getBookSearchConfig('', user))
 }
 
-export {useBook, useBookSearch, refetchBookSearchQuery}
+export {useBook, useBookSearch, refetchBookSearchQuery, setQueryDataForBook}

0e9be455addb51a85ccbccdd31b0d9ff3ee3da96
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-09-22 21:32:50 +0200

    7.2 create an AuthProvider component

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,6 +1,14 @@
 import * as React from 'react'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
+
+import {FullPageSpinner} from 'components/lib'
+import {useAsync} from 'utils/hooks'
+import {client} from 'utils/api-client'
+import * as colors from 'styles/colors'
 
 const AuthContext = React.createContext()
+AuthContext.displayName = 'AuthContext'
 
 function useAuth() {
   const context = React.useContext(AuthContext)
@@ -10,4 +18,68 @@ function useAuth() {
   return context
 }
 
-export {useAuth, AuthContext}
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    queryCache.clear()
+    setData(null)
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return <AuthContext.Provider value={value} {...props} />
+  }
+}
+
+export {useAuth, AuthProvider}

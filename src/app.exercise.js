a461bda54d45bf7bf82565a9f3025c0f2b365d9c
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-09-23 20:01:27 +0200

    7.3 - colocate global providers

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,20 +2,12 @@
 import {jsx} from '@emotion/core'
 import {useAuth} from 'context/auth-context.exercise'
 
-import {BrowserRouter as Router} from 'react-router-dom'
-
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
 function App() {
   const {user} = useAuth()
-  return user ? (
-    <Router>
-      <AuthenticatedApp />
-    </Router>
-  ) : (
-    <UnauthenticatedApp />
-  )
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
 }
 
 export {App}

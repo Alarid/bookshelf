72b756a1cb3b3a4a6589198c6ca59d829262d979
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-10-05 19:17:12 +0200

    13.7 - write error state tests

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -12,6 +12,9 @@ import {App} from 'app'
 import * as listItemsDB from 'test/data/list-items'
 import * as booksDB from 'test/data/books'
 import {formatDate} from 'utils/misc'
+import {server, rest} from 'test/server'
+
+const apiURL = process.env.REACT_APP_API_URL
 
 async function renderBookScreen({user, book, listItem} = {}) {
   user = typeof user === 'undefined' ? await loginAsUser() : user
@@ -144,5 +147,47 @@ test('can edit a note', async () => {
   expect(notesTextarea).toHaveValue(newNotes)
   expect(await listItemsDB.read(listItem.id)).toMatchObject({
     notes: newNotes,
+  })
+})
+
+describe('Error cases', () => {
+  beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+  afterAll(() => {
+    console.error.mockRestore()
+  })
+
+  test('shows an error message when the book fails to load', async () => {
+    await renderBookScreen({book: {id: 'wrong'}, listItem: null})
+    expect(
+      (await screen.findByRole('alert')).textContent,
+    ).toMatchInlineSnapshot(`"There was an error: Book not found"`)
+  })
+
+  test('note update failures are displayed', async () => {
+    jest.useFakeTimers()
+    const {listItem} = await renderBookScreen()
+
+    server.use(
+      rest.put(`${apiURL}/list-items/:listItemId`, async (req, res, ctx) =>
+        res(ctx.status(400), ctx.json({status: 400, message: 'Oh no!'})),
+      ),
+    )
+
+    const newNotes = faker.lorem.words()
+    const notesTextarea = screen.getByRole('textbox', {name: /notes/i})
+
+    userEvent.clear(notesTextarea)
+    userEvent.type(notesTextarea, newNotes)
+    await screen.findByLabelText(/loading/i)
+    await waitForLoadingToFinish()
+
+    expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+      `"There was an error: Oh no!"`,
+    )
+    expect(await listItemsDB.read(listItem.id)).not.toMatchObject({
+      notes: newNotes,
+    })
   })
 })
